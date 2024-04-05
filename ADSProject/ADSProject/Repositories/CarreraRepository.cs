b4d0623c5f1a3796e894c342ad1f9ab8526e1e8b
Gaby <130328119+PimentelGaby@users.noreply.github.com>
2024-04-05 15:32:57 -0600

    GUIA 2
    Subiendo GUIA 2

---
+++ ADSProject/ADSProject/Repositories/CarreraRepository.cs
@@ -0,0 +1,99 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ADSProject.Repositories
+{
+    public class CarreraRepository : ICarrera
+    {
+        private List<Carrera> lstCarreras = new List<Carrera>
+        {
+            new Carrera
+            {
+                IdCarrera = 1,
+                Codigo = "i04",
+                Nombre = "Ingeniería en Sistemas Commputacionales"
+            },
+            new Carrera
+            {
+                IdCarrera = 2,
+                Codigo = "i03",
+                Nombre = "Ingeniería Industrial"
+            }
+        };
+
+        public int ActualizarCarrera(int idCarrera, Carrera carrera)
+        {
+            try
+            {
+                int indice = lstCarreras.FindIndex(tmp => tmp.IdCarrera == idCarrera);
+                lstCarreras[indice] = carrera;
+                return idCarrera;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int AgregarCarrera(Carrera carrera)
+        {
+            try
+            {
+                if (lstCarreras.Count > 0)
+                {
+                    carrera.IdCarrera = lstCarreras.Last().IdCarrera + 1;
+                }
+
+                lstCarreras.Add(carrera);
+
+                return carrera.IdCarrera;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool EliminarCarrera(int idCarrera)
+        {
+            try
+            {
+                int indice = lstCarreras.FindIndex(tmp => tmp.IdCarrera == idCarrera);
+                lstCarreras.RemoveAt(indice);
+                return true;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Carrera ObtenerCarreraPorID(int idCarrera)
+        {
+            try
+            {
+                Carrera carrera = lstCarreras.FirstOrDefault(tmp => tmp.IdCarrera == idCarrera);
+                return carrera;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public List<Carrera> ObtenerTodasLasCarreras()
+        {
+            try
+            {
+                return lstCarreras;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ ADSProject/ADSProject/Repositories/EstudianteRepository.cs
@@ -0,0 +1,103 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+
+namespace ADSProject.Repositories
+{
+    public class EstudianteRepository : IEstudiante
+    {
+        private List<Estudiante> lstEstudiantes = new List<Estudiante>
+        {
+            new Estudiante
+            {
+                IdEstudiante = 1,
+                NombresEstudiante = "ADOLFO CORTEZ",
+                ApellidosEstudiante = "CORTEZ BARRERA",
+                CodigoEstudiante = "CB21I04001",
+                CorreoEstudiante = "cb21i04001@usonsonate.edu.sv"
+            }
+        };
+        public int ActualizarEstudiante(int idEstudiante, Estudiante estudiante)
+        {
+            try
+            {
+                // Obtenemos el indice del objeto para actualizar
+                int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == idEstudiante);
+                // Procedemos con la actualizacion
+                lstEstudiantes[indice] = estudiante;
+                return idEstudiante;
+
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int AgregarEstudiante(Estudiante estudiante)
+        {
+            try
+            {
+                // Validar si existen datos en la lista, de ser asi, tomaremos el ultimo ID
+                // y lo incrementamos en una unidad
+                if (lstEstudiantes.Count > 0)
+                {
+                    estudiante.IdEstudiante = lstEstudiantes.Last().IdEstudiante + 1;
+                }
+
+                lstEstudiantes.Add(estudiante);
+
+                return estudiante.IdEstudiante;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool EliminarEstudiante(int idEstudiante)
+        {
+            try
+            {
+                // Obtenemos el indice el objeto a eliminar
+                int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == idEstudiante);
+                // Procedemos a eliminar el registro
+                lstEstudiantes.RemoveAt(indice);
+                return true;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Estudiante ObtenerEstudiantePorID(int idEstudiante)
+        {
+            try
+            {
+                // Buscamos y asignamos el objeto estudiante
+                Estudiante estudiante = lstEstudiantes.FirstOrDefault(tmp => tmp.IdEstudiante == idEstudiante);
+               
+                return estudiante;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public List<Estudiante> ObtenerTodosLosEstudiantes()
+        {
+            try
+            {
+                return lstEstudiantes;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
